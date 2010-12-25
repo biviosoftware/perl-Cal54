516df4dc172070be1bc7d93a5dd4ca056625873d
Paul Moeller <moeller@bivio.biz>
2010-12-25 22:50:56 +0000

    added clear_calendar_events() and import_ics()

--- Util/SQL.pm
+++ Util/SQL.pm
@@ -11,8 +11,28 @@ use Bivio::Base 'ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
+sub clear_calendar_events {
+    my($self) = @_;
+    $self->usage_error('Can not be run with a production database.')
+	if $self->req->is_production;
+    $self->model('CalendarEvent')->do_iterate(sub {
+        my($ce) = @_;
+	$ce->cascade_delete;
+	return 1;
+    }, 'unauth_iterate_start');
+    return;
+}
+
 sub ddl_files {
     return shift->SUPER::ddl_files(['bOP', 'c4']);
+}
+
+sub import_ics {
+    my($self) = @_;
+    $self->assert_not_general;
+    $self->initialize_ui;
+    $self->model('CalendarEvent')->update_from_ics($self->read_input);
+    return;
 }
 
 sub initialize_db {
