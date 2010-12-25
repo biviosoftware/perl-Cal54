3ad55a77842b2be8592859a1c9710f7399908544
Rob Nagler <github@q33.us>
2010-12-25 22:48:54 +0000

    added reset_all

--- Util/TestData.pm
+++ Util/TestData.pm
@@ -11,12 +11,34 @@ sub USAGE {
 usage: bivio TestData [options] command [args..]
 commands
   init -- initializes test data
+  reset_all - deletes all test venues and events
 EOF
 }
 
 sub init {
     my($self) = @_;
+    $self->assert_test;
     $self->new_other('TestUser')->init;
+    return;
+}
+
+sub reset_all {
+    my($self) = @_;
+    $self->assert_test;
+    foreach my $type (qw(CALENDAR_EVENT VENUE)) {
+	$self->model('RealmOwner')
+	    ->do_iterate(
+		sub {
+		    my($it) = @_;
+		    $self->model('Venue')->unauth_delete_realm($it)
+			if $it->get('display_name') =~ /\btest\b/i;
+		    return 1;
+		},
+		'unauth_iterate_start',
+		'realm_id',
+		{realm_type => [$type]},
+	    );
+    }
     return;
 }
 
