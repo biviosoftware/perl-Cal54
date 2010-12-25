4fe8051b2a978098be137c28c8bedc6b78f43ddc
Rob Nagler <github@q33.us>
2010-12-25 21:58:44 +0000

    more delegates

--- BConf.pm
+++ BConf.pm
@@ -16,19 +16,18 @@ sub merge_overrides {
     my($proto, $host) = @_;
     return Bivio::IO::Config->merge_list({
 	$proto->merge_class_loader({
-	    delegates => {
-		'Bivio::Agent::HTTP::Cookie' => 'Bivio::Delegate::Cookie',
-		'Bivio::Agent::TaskId' => 'Cal54::Delegate::TaskId',
-		'Bivio::Auth::Support' => 'Bivio::Delegate::SimpleAuthSupport',
-		'Bivio::TypeError' => 'Cal54::Delegate::TypeError',
-	    },
+	    delegates => [
+		'Bivio::Agent::TaskId',
+		'Bivio::Auth::RealmType',
+                'Bivio::Type::Location',
+	    ],
 	    maps => {
-#		Action => ['Cal54::Action'],
 		Delegate => ['Cal54::Delegate'],
 		Facade => ['Cal54::Facade'],
 		Model => ['Cal54::Model'],
 		ShellUtil => ['Cal54::Util'],
 		TestLanguage => ['Cal54::Test'],
+		XHTMLWidget => ['Cal54::XHTMLWidget'],
 		Type => ['Cal54::Type'],
 		View => ['Cal54::View'],
 	    },
