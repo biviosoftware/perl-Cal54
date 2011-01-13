e2cf229821d6d835eefcc7ce0fcb79bebe60b0f0
Paul Moeller <moeller@bivio.biz>
2011-01-13 03:04:53 +0000

    added clear_events() and import_events() removed import_ics()

--- Util/CalendarEvent.pm
+++ Util/CalendarEvent.pm
@@ -5,26 +5,21 @@ use strict;
 use Bivio::Base 'Bivio.ShellUtil';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-my($_D) = b_use('Type.Date');
-my($_MC) = b_use('MIME.Calendar');
-my($_RR) = b_use('MIME.RRule');
-my($_TZ) = b_use('Type.TimeZone');
+my($_DT) = b_use('Type.DateTime');
 
 sub USAGE {
     return <<'EOF';
 usage: bivio CalendarEvent [options] command [args..]
 commands
-  import_ics [time_zone] -- reads ICS from input and imports into auth_realm
+  clear_events -- clear all events for a venue
+  import_events -- scrape and import events for a venue
 EOF
 }
 
-sub import_ics {
-    my($self, $time_zone) = @_;
-    $self->assert_not_general;
-    $self->initialize_ui;
-    my($tz) = $time_zone ? $_TZ->from_any($time_zone) : undef;
-    # clear the venue's existing events
-#TODO: reuse calendar_event_id    
+sub clear_events {
+    my($self) = @_;
+    $self->are_you_sure('Clear events for '
+        . $self->req(qw(auth_realm owner name)) . '?');
     my($ro) = $self->model('RealmOwner');
     $self->model('CalendarEvent')->do_iterate(sub {
         my($ce) = @_;
@@ -36,49 +31,19 @@ sub import_ics {
 	});
  	return 1;
     });
-    my($start) = $_D->add_days($_D->local_today, -1);
-    my($end) = $_D->add_months($_D->local_today, 3);
-    my($recurrences) = {};
-    
-    foreach my $vevent (reverse(@{$_MC->from_ics($self->read_input)})) {
-
-	if ($vevent->{'recurrence-id'}) {
-	    $recurrences->{_recurrence_id($vevent, 'recurrence-id')} = 1;
-	}
-	next if $_D->is_date($vevent->{dtstart});
-	next if ($vevent->{status} || '') eq 'CANCELLED';
-	next unless ($vevent->{class} || 'PUBLIC') eq 'PUBLIC';
-
-	foreach my $v (@{_explode_event($self, $vevent, $end)}) {
-	    next if $_D->compare($v->{dtstart}, $start) < 0;
-
-	    if ($v->{rrule} && $recurrences->{_recurrence_id($v)}) {
-		next;
-	    }
-	    my($ce) = $self->model('CalendarEvent')->create_from_vevent($v);
-	    $ce->update({
-		time_zone => $tz,
-	    }) if $tz;
-	}
-    }
     return;
 }
 
-sub _explode_event {
-    my($self, $vevent, $end) = @_;
-    return [$vevent] unless $vevent->{rrule};
-    return [
-	map(+{
-	    %$vevent,
-	    %$_,
-	}, @{$_RR->process_rrule($vevent, $end)}),
-    ];
-}
-
-sub _recurrence_id {
-    my($vevent, $date_field) = @_;
-    return join('-',
-        map($vevent->{$_}, qw(uid sequence), $date_field || 'dtstart'));
+sub import_events {
+    my($self) = @_;
+    $self->initialize_ui;
+    my($list) = $self->model('VenueList')->unauth_load_all;
+    $list->find_row_by('RealmOwner.display_name',
+        $self->req(qw(auth_realm owner display_name)))
+	|| $self->usage_error('venue not found: ',
+            $self->req(qw(auth_realm owner display_name)));
+    b_use('Bivio.Scraper')->do_one($list, $_DT->now);
+    return;
 }
 
 1;
