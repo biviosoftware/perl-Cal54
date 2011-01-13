b69ed84ab7f590021500e13613288673acea9142
Rob Nagler <github@q33.us>
2011-01-13 06:22:04 +0000

    added event editing

--- XHTMLWidget/SiteAdminDropDown.pm
+++ XHTMLWidget/SiteAdminDropDown.pm
@@ -10,7 +10,8 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub TASK_MENU_LIST {
     return (
 	shift->SUPER::TASK_MENU_LIST(@_),
-	'venue_list',
+	'adm_venue_list',
+	'adm_calendar_event_list_form',
     );
 }
 
