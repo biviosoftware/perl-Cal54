df6a511008601860324fe9f3d24f3f1b5d7d8d3a
Paul Moeller <moeller@bivio.biz>
2011-01-14 17:58:14 +0000

    added rss link lookup, cache xml data by url for use across venues

--- Scraper/ActiveData.pm
+++ Scraper/ActiveData.pm
@@ -9,79 +9,55 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_D) = b_use('Type.Date');
 my($_DT) = b_use('Type.DateTime');
 my($_HTML) = b_use('Bivio::HTML');
+# cache xml to be shared across venues
+my($_XML_BY_URI_KEY) = __PACKAGE__ . 'xml';
 
 sub internal_import {
     my($self) = @_;
-    my($addr1) = $self->get('venue_list')->get('Address.street1');
-    b_die('venue missing Address.street1')
-	unless $addr1;
     my($start) = $_D->add_days($_D->local_today, -1);
     my($end) = $_D->add_months($start, 6);
-    # first parse calendar for "/eventlistsyndicator.aspx", use that host
-    # otherwise look for "EventList.aspx" and use current host
     my($webhost) = _parse_webhost($self);
-    # query the Eventlist.aspx xml download
-    # http://events.colorado.edu/Eventlist.aspx?fromdate=1/1/2011&todate=1/31/2011&type=public&download=download&dlType=XML
-    my($xml) = $self->c4_scraper_get('http://' . $webhost . '/Eventlist.aspx?'
-        . join('&', 				       
-	       'fromdate=' . $_D->to_string($start),
-	       'todate=' . $_D->to_string($end),
-	       'type=public',
-	       'download=download',
-	       'dlType=XML'
-	));
-    my($res, $err) = XML::Simple::xml_in($$xml,
-        NoAttr => 1,
-	SuppressEmpty => undef);
-    b_die('xml parse error: ', $err) if $err;
-    b_die('no events: ', $res) unless keys(%$res);
-    
-    # iterate events, taking Address1 which match the current venue
-    foreach my $event (@{$res->{Event}}) {
-	next unless lc($addr1) eq lc($event->{Address1} || '');
-	next if lc($event->{Status} || '') eq 'cancelled';
-	next unless $event->{StartDate}
-	    && $event->{StartTime}
-	    && $event->{EndDate}
-	    && $event->{EndTime};
-	push(@{$self->get('events')}, {
-	    summary => _clean($self, $event->{EventName}),
-	    time_zone => $self->get('time_zone'),
-	    description => _clean($self, $event->{EventDescription}),
-	    dtstart => _date_time($self, $event, 'Start'),
-	    dtend => _date_time($self, $event, 'End'),
-	});
+    return unless _parse_event_xml($self, $webhost, $start, $end);
+    _add_event_urls($self, $webhost, $start, $end);
+    return;
+}
 
-	# description combines EventDescription, Location, Building, Room,
-	# ContactName, ContactPhone, ContactEmail,
-	# ExternalField1, ExternalField2
+sub _add_event_urls {
+    my($self, $webhost, $start, $end) = @_;
+    my($xml) = _parse_xml($self, 'http://' . $webhost . '/RSSSyndicator.aspx?'
+	. join('&',
+	    'category=',
+	    'location=',
+	    'type=N',
+	    'starting=' . $_D->to_string($start),
+	    'ending=' . $_D->to_string($end),
+	    'binary=Y'));
 
-	# EventName: Performance Certificate Recital: Margaret Higginson...
-	# EventDescription: &lt;div&gt;PROGRAM:&lt;/div&gt;...
-	# ContactName: CU Presents Box Office
-	# ContactPhone: 303-492-8008
-	# ContactEmail: musictix@colorado.edu
-	# Location: Main Campus
-	# Building: Imig Music
-	# Room: Grusin Music Hall
-	# ExternalField1: &lt;li&gt;Everyone&lt;/li&gt;
-	# ExternalField2: Free and open to the public. No tickets required.
-	# StartDate: 1/12/2011
-	# StartTime: 07:30 PM
-	# EndDate: 1/12/2011
-	# EndTime: 09:30 PM
-        # OneTimeSeries: One Time
-	# Status: Cancelled
-	# Address1: 1020 18th Street
-	# Address2:
-	# City: Boulder
-	# State: CO
-	# Zipcode:
-	# Phone:
+    unless ($xml->{channel} && $xml->{channel}->{item}) {
+	b_warn('missing channel/item');
+	return;
+    }
+    my($links_by_start_time) = {};
+
+    foreach my $item (@{$xml->{channel}->{item}}) {
+	# pubDate has the GMT start time
+	my($title) = _strip_trailing_parens($self,
+            _clean($self, $item->{title}));
+	($links_by_start_time->{$_DT->from_literal_or_die($item->{pubDate})}
+	    ||= {})->{$title} = $item->{link};
     }
 
-    # download rss and get detail links for each event
-    # http://events.colorado.edu/RSSSyndicator.aspx?category=&location=5-312-0&type=N&starting=1/1/2011&ending=1/31/2011&binary=Y
+    foreach my $event (@{$self->get('events')}) {
+	my($dtstart) = $event->{dtstart};
+	my($summary) = _strip_trailing_parens($self, $event->{summary});
+
+	unless ($links_by_start_time->{$dtstart}
+	    && $links_by_start_time->{$dtstart}->{$summary}) {
+	    b_warn('missing url for event: ', $dtstart, ' ', $summary);
+	    next;
+	}
+	$event->{url} = $links_by_start_time->{$dtstart}->{$event->{summary}};
+    }
     return;
 }
 
@@ -104,18 +80,74 @@ sub _date_time {
 	$_DT->from_parts_or_die(0, $min, $hour, $mday, $mon, $year));
 }
 
+sub _parse_event_xml {
+    my($self, $webhost, $start, $end) = @_;
+    my($addr1) = $self->get('venue_list')->get('Address.street1');
+    b_die('venue missing Address.street1')
+	unless $addr1;
+    my($xml) = _parse_xml($self, 'http://' . $webhost . '/Eventlist.aspx?'
+        . join('&', 				       
+	    'fromdate=' . $_D->to_string($start),
+	    'todate=' . $_D->to_string($end),
+	    'type=public',
+	    'download=download',
+	    'dlType=XML'
+	));
+
+    # iterate events, taking Address1 which match the current venue
+    foreach my $event (@{$xml->{Event}}) {
+	next unless lc($addr1) eq lc($event->{Address1} || '');
+	next if lc($event->{Status} || '') eq 'cancelled';
+	next if ($event->{ExternalField1} || '') =~ /students|alumni/i;
+	next unless $event->{StartDate} && $event->{StartTime}
+	    && $event->{EndDate} && $event->{EndTime};
+	push(@{$self->get('events')}, {
+	    summary => _clean($self, $event->{EventName}),
+	    time_zone => $self->get('time_zone'),
+	    description => _clean($self, $event->{EventDescription}),
+	    dtstart => _date_time($self, $event, 'Start'),
+	    dtend => _date_time($self, $event, 'End'),
+	});
+    }
+    return @{$self->get('events')} ? 1 : 0;
+}
+
 sub _parse_webhost {
     my($self) = @_;
+    # first parse calendar for "/eventlistsyndicator.aspx", use that host
+    # otherwise look for "EventList.aspx" and use current host
     my($html) = $self->c4_scraper_get(
 	$self->get('venue_list')->get('calendar.Website.url'));
-    my($host) = $$html =~ m,http://(.*?)/EventListSyndicator.aspx,;
+    my($host) = $$html =~ m,://(.*?)/(EventListSyndicator|displaymedia).aspx,;
     return $host if $host;
     b_die('unparsed host value')
 	unless $$html =~ /("|')EventList.aspx\?/;
     $host = $$html =~ m,http://(.*?)/,;
-    b_die('host not found in calendar website')
-	unless $host;
-    return $host;
+    return $host || b_die('host not found in calendar website');
+}
+
+sub _parse_xml {
+    my($self, $url) = @_;
+    # many venues can share the same xml ActiveData source, cache by url
+    $self->req->put_unless_exists($_XML_BY_URI_KEY => {});
+    return $self->req($_XML_BY_URI_KEY)->{$url}
+	if $self->req($_XML_BY_URI_KEY)->{$url};
+    my($xml, $err) = XML::Simple::xml_in(${$self->c4_scraper_get($url)},
+        NoAttr => 1,
+	SuppressEmpty => undef);
+    b_die('xml parse error: ', $err) if $err;
+    b_die('no xml data for url: ', $url) unless keys(%$xml);
+    $self->req($_XML_BY_URI_KEY)->{$url} = $xml;
+    return $xml;
+}
+
+sub _strip_trailing_parens {
+    my($self, $value) = @_;
+
+    while ($value =~ m,\(.*?\)\s*$,) {
+	$value =~ s,\s*\(.*?\)\s*$,,;
+    }
+    return $value;
 }
 
 1;
