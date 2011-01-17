3bc6353e70de367569ab3eb2203fb1709dc6706b
Paul Moeller <moeller@bivio.biz>
2011-01-17 22:22:33 +0000

    reimplemented with direct HTML::Parser, correctly handles spaces

--- HTMLCleaner.pm
+++ HTMLCleaner.pm
@@ -2,48 +2,13 @@
 # $Id$
 package Cal54::HTMLCleaner;
 use strict;
-use Bivio::Base 'HTML.Scraper';
+use Bivio::Base 'Bivio::UNIVERSAL';
+use HTML::Parser ();
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_IDI) = __PACKAGE__->instance_data_index;
 my($_HTML) = b_use('Bivio.HTML');
 my($_S) = b_use('Type.String');
-my($_IGNORE_TAG_UNTIL_END) = {
-    map(($_ => 1), qw(
-        script
-	noscript
-	object
-	xml
-	style
-    )),
-};
-my($_IGNORE_START_TAG) = {
-    map(($_ => 1), qw(
-        input
-	link
-	img
-	form
-	tr
-	td
-	div
-	span
-	font
-	b
-	p
-	strong
-	a
-	center
-	big
-	h1
-	h2
-	h3
-	h4
-	li
-	small
-	tbody
-	em
-    )),
-};
 my($_END_NEWLINE_TAG) = {
     map(($_ => 1), qw(
         tr
@@ -54,106 +19,118 @@ my($_END_NEWLINE_TAG) = {
 	h2
 	h3
 	li
+	td
     )),
 };
 my($_START_NEWLINE_TAG) = {
     map(($_ => 1), qw(
+        p			 
         br
         hr
 	ul
+	table
     )),
 };
 
 sub clean_html {
-    my($proto, $html) = @_;
-    my($self) = $proto->new;
-    my($fields) = $self->[$_IDI] = {
-    	in_body => 0,
-    	wait_for_end_tag => '',
-    	table_depth => 0,
-    	text => '',
-    };
-    $self->parse_html($html);
-    return \($fields->{text});
+    my($self, $html) = @_;
+    my($fields) = $self->[$_IDI];
+    my($parser) = b_use('Ext.HTMLParser')->new($self);
+    $parser->{__PACKAGE__} = $self;
+    # register text handler, includes whitespace
+    $parser->handler(text => \&_parse_text, 'self,text');
+    $parser->ignore_elements(qw(script noscript object style xml));
+    {
+	# ignore utf warnings
+	local($SIG{__WARN__}) = sub {};
+        $parser->parse($$html);
+    }
+    $fields->{text} =~ s/( )+$//mg;
+    delete($parser->{__PACKAGE__});
+    return \($fields->{text} . "\n");
+}
+
+sub get_link_for_text {
+    my($self, $text) = @_;
+    my($fields) = $self->[$_IDI];
+    $text =~ s/^\s+|\s+$//g;
+    my($links) = $fields->{links}->{$text};
+
+    unless ($links) {
+	b_warn('no links for text: ', $text);
+	return undef;
+    }
+    if (@$links > 2) {
+	b_warn('multiple links for text: ', $text);
+    }
+    return $links->[0];
+}
+
+sub html_parser_comment {
+    return;
 }
 
 sub html_parser_end {
     my($self, $tag) = @_;
     my($fields) = $self->[$_IDI];
-    return unless $fields->{in_body};
 
-    if ($tag eq 'body') {
-	$fields->{in_body} = 0;
-	return;
-    }
-    if ($fields->{wait_for_end_tag}) {
-	if ($tag eq $fields->{wait_for_end_tag}) {
-	    $fields->{wait_for_end_tag} = '';
-	}
-	return;
-    }
-    if ($tag eq 'table') {
-	$fields->{table_depth} = $fields->{table_depth} - 1;
-	b_die() if $fields->{table_depth} < 0;
-	return;
-    }
     if ($_END_NEWLINE_TAG->{$tag}) {
-	_append($self, "\n");
-	_append($self, "\n")
+	$fields->{text} .= "\n";
+	$fields->{text} .= "\n"
 	    if $tag eq 'p';
-	return;
     }
-    if ($tag eq 'td') {
-	_append($self, '  ');
-	return;
+    if ($tag eq 'a') {
+	return unless $fields->{link_text} && $fields->{href};
+	$fields->{link_text} =~ s/^\s+|\s+$//g;
+	push(@{$fields->{links}->{$fields->{link_text}} ||= []},
+	     $fields->{href});
+	$fields->{href} = $fields->{link_text} = undef;
     }
     return;
 }
 
 sub html_parser_start {
-    my($self, $tag, $attr) = @_;
+    my($self, $tag, $attrs) = @_;
     my($fields) = $self->[$_IDI];
-    return if $fields->{wait_for_end_tag};
-    return if $_IGNORE_START_TAG->{$tag};
 
-    if ($tag eq 'body') {
-	b_die('body already found')
-	    if $fields->{in_body};
-	$fields->{in_body} = 1;
-	return;
-    }
-    return unless $fields->{in_body};
-
-    if ($_IGNORE_TAG_UNTIL_END->{$tag}) {
-	$fields->{wait_for_end_tag} = $tag;
-	return;
-    }
-    if ($tag eq 'table') {
-	$fields->{table_depth} = $fields->{table_depth} + 1;
-	return;
-    }
     if ($_START_NEWLINE_TAG->{$tag}) {
-	_append($self, "\n");
-	return;
+	$fields->{text} .= "\n";
     }
-    return unless $tag =~ /^\w+$/;
-    b_warn('unhandled tag: ', $tag);
+    if ($tag eq 'a' && $attrs->{href}) {
+	$fields->{href} = $attrs->{href};
+    }
     return;
 }
 
-sub html_parser_text {
-    my($self, $text) = @_;
-    my($fields) = $self->[$_IDI];
-    return unless $fields->{in_body};
-    return if $fields->{wait_for_end_tag};
-    _append($self, ${$_S->canonicalize_charset($_HTML->unescape($text))});
-    return;
+sub new {
+    my($self) = shift->SUPER::new(@_);
+    $self->[$_IDI] = {
+    	in_body => 0,
+    	wait_for_end_tag => '',
+    	table_depth => 0,
+    	text => '',
+	links => {},
+    };
+    return $self;
 }
 
-sub _append {
-    my($self, $text) = @_;
+sub _parse_text {
+    my($parser, $text) = @_;
+    my($self) = $parser->{__PACKAGE__};
     my($fields) = $self->[$_IDI];
-    $fields->{text} .= $text;
+    my($leading_white) = $text =~ /^(\s+)/;
+    my($trailing_white) = $text =~ /\S(\s+)$/;
+    my($value) = join('',
+	$fields->{text} =~ /\s$/ || $fields->{text} eq ''
+	    ? ''
+	    : ($leading_white ? ' ' : ''),
+	${$_S->canonicalize_charset($_HTML->unescape($text))},
+	$trailing_white ? ' ' : '',
+    );
+    if ($fields->{href}) {
+	$fields->{link_text} .= $value;
+    }
+    $fields->{text} .= $value;
     return;
 }
 
