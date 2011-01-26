b274a1cd482e559647c4522589a2651e8f833d3f
Paul Moeller <moeller@bivio.biz>
2011-01-26 18:19:56 +0000

    initial revision

---
+++ Action/ScraperPreview.pm
@@ -0,0 +1,24 @@
+# Copyright (c) 2011 bivio Software Inc.  All Rights Reserved.
+# $Id$
+package Cal54::Action::ScraperPreview;
+use strict;
+use Bivio::Base 'Biz.Action';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub execute {
+    my($self) = shift->new(@_);
+    $self->put(text => '');
+    my($uri) = ($self->req('query') || {})->{x};
+    return unless $uri;
+    my($scraper) = b_use('HTML.Scraper')->new;
+    my($cleaner) = b_use('Bivio.HTMLCleaner')->new;
+    $self->put(
+	cleaner => $cleaner,
+	text => ${$cleaner->clean_html(
+	    $scraper->extract_content($scraper->http_get($uri)), $uri)},
+    );
+    return;
+}
+
+1;
