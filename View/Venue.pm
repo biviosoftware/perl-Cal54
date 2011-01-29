277dafc8cabb24ca9100df6bc7d992b11019e7a3
Paul Moeller <moeller@bivio.biz>
2011-01-29 21:45:17 +0000

    allow testing a scraper

--- View/Venue.pm
+++ View/Venue.pm
@@ -36,15 +36,37 @@ sub list {
 }
 
 sub scraper {
-    return shift->internal_body(
-	vs_simple_form(VenueScraperForm => [
-	    H1(String([['Model.Venue', '->get_model', 'RealmOwner'],
-		       'display_name'])),
-	    'VenueScraperForm.Venue.scraper_type',
-	    ['VenueScraperForm.Venue.scraper_aux', {
-		cols => 80,
-		rows => 25,
-	    }],
+    return shift->internal_put_base_attr(
+	tools => TaskMenu([
+	    Link('Venue Calendar',
+		 ['Model.VenueScraperForm', 'calendar.Website.url']),
+	    Link('Scraper Calendar', URI({
+		task_id => 'ADM_SCRAPER_PREVIEW',
+		query => {
+		    x => ['Model.VenueScraperForm', 'calendar.Website.url'],
+		},
+	    })),
+	    'ADM_VENUE_LIST',
+	]),
+	body => Join([
+	    vs_simple_form(VenueScraperForm => [
+		H1(String([['Model.Venue', '->get_model', 'RealmOwner'],
+		    'display_name'])),
+		'VenueScraperForm.Venue.scraper_type',
+		['VenueScraperForm.Venue.scraper_aux', {
+		    cols => 80,
+		    rows => 25,
+		}],
+		'*ok_button test_scraper_button cancel_button',
+	    ]),
+	    DIV_c4_scraper(If(['Model.VenueScraperForm', '->has_events'],
+	       String([sub {
+                   my($source) = @_;
+		   return ${b_use('IO.Ref')->to_string(
+		       $source->req(qw(Model.VenueScraperForm events)))};
+	       }])->put(hard_newlines => 1),
+	    )),
+	    String(['Model.VenueScraperForm', 'error']),
 	]),
     );
 }
