1ac1f47b7d7208d70fb3fd0ede6dfff3e68cfea1
Rob Nagler <github@q33.us>
2011-03-03 22:47:18 +0000

    *** empty log message ***

---
+++ Model/PopularList.pm
@@ -0,0 +1,43 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Cal54::Model::PopularList;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    return {
+	version => 1,
+	primary_key => [
+	    {
+		name => 'item',
+		type => 'Line',
+		constraint => 'NOT_NULL',
+	    },
+	],
+    };
+}
+
+sub internal_load_rows {
+    my($self, $query, $where, $params, $sql_support) = @_;
+    return [map(
+	+{item => $_},
+	qw(
+	    Music
+	    Free
+	    Kids
+	    Jazz
+	),
+    )];
+}
+
+sub search_query {
+    my($self) = @_;
+    return {
+	what => $self->get('item'),
+	when => $self->req('Model.HomeList')->get_query->unsafe_get('when'),
+    };
+}
+
+1;
---
+++ Model/WhenList.pm
@@ -0,0 +1,43 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Cal54::Model::WhenList;
+use strict;
+use Bivio::Base 'Biz.ListModel';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_initialize {
+    return {
+	version => 1,
+	primary_key => [
+	    {
+		name => 'item',
+		type => 'Line',
+		constraint => 'NOT_NULL',
+	    },
+	],
+    };
+}
+
+sub internal_load_rows {
+    my($self, $query, $where, $params, $sql_support) = @_;
+    return [map(
+	+{item => $_},
+	qw(
+	    Today
+	    Tomorrow
+	    Friday
+	    Saturday
+	),
+    )];
+}
+
+sub search_query {
+    my($self) = @_;
+    return {
+	when => $self->get('item'),
+	what => $self->req('Model.HomeList')->get_query->unsafe_get('what'),
+    };
+}
+
+1;
