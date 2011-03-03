b42ad19cd94b9c0e34317c075a0bfc07d1e981e9
Rob Nagler <github@q33.us>
2011-03-03 22:47:10 +0000

    removed where

--- Model/HomeQueryForm.pm
+++ Model/HomeQueryForm.pm
@@ -6,19 +6,8 @@ use Bivio::Base 'Model.ListQueryForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 
-sub execute_empty {
-    my($self) = @_;
-    my(@res) = shift->SUPER::execute_empty(@_);
-    $self->internal_put_field(
-	where => 'Boulder',
-	when => 'now',
-    );
-    return @res;
-}
-
 sub internal_query_fields {
     return [
-	[qw(where Line)],
 	[qw(what Line)],
 	[qw(when Line)],
     ];
