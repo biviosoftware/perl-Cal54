378d9cc75c8a4f3ee45b991fcfeeb1a15e4d46ae
Paul Moeller <moeller@bivio.biz>
2011-03-07 03:49:11 +0000

    Quit
    -UU-:----F1  cvsZWQphC      All L1     (Fundamental)---------------------------
    Warning (server): Unable to start the Emacs server.
    There is an existing Emacs server, named "server".
    To start the server in this Emacs process, stop the existing
    server or call `M-x server-force-delete' to forcibly disconnect it.
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    -UUU:**--F1  *Warnings*     All L5     (Fundamental)---------------------------
    For information about GNU Emacs and the GNU system, type C-h C-a.
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    rename

--- Model/ScraperForm.pm
+++ Model/ScraperForm.pm
@@ -1,21 +1,48 @@
 # Copyright (c) 2011 bivio Software Inc.  All Rights Reserved.
 # $Id$
-package Cal54::Model::VenueScraperForm;
+package Cal54::Model::ScraperForm;
 use strict;
-use Bivio::Base 'Biz.FormModel';
+use Bivio::Base 'Model.FormModeBaseForm';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_DT) = b_use('Type.DateTime');
 
-sub execute_empty {
+sub LIST_MODEL {
+    return 'ScraperList';
+}
+
+sub PROPERTY_MODEL {
+    return 'Scraper';
+}
+
+sub execute_empty_create {
     my($self) = @_;
-    $self->load_from_model_properties($self->req('Model.Venue'));
     return;
 }
 
-sub execute_ok {
+sub execute_empty_edit {
     my($self) = @_;
-    $self->update_model_properties($self->req('Model.Venue'));
+    $self->load_from_model_properties($self->req('Model.Scraper'));
+    $self->load_from_model_properties(_website($self));
+    return;
+}
+
+sub execute_ok_create {
+    my($self) = @_;
+    my($scraper) = $self->new_other('Scraper')->create_realm(
+	$self->get_model_properties('Scraper'),
+	{});
+    $self->new_other('Website')->create({
+	realm_id => $scraper->get('scraper_id'),
+	%{$self->get_model_properties('Website')},
+    });
+    return;
+}
+
+sub execute_ok_edit {
+    my($self) = @_;
+    $self->update_model_properties($self->req('Model.Scraper'));
+    $self->update_model_properties(_website($self));
     return;
 }
 
@@ -23,12 +50,11 @@ sub execute_other {
     my($self) = @_;
     $self->validate_and_execute_ok;
     return if $self->in_error;
-    my($list) = $self->new_other('VenueList')->unauth_load_all;
-    $list->find_row_by('Venue.venue_id', $self->req(qw(Model.Venue venue_id)))
-	|| b_die('venue not in VenueList');
+    my($list) = $self->new_other('ScraperList')->unauth_load_all;
+    $list->find_row_by('Scraper.scraper_id', $self->req(qw(Model.Scraper scraper_id)))
+	|| b_die('scraper not in ScraperList');
     my($die) = Bivio::Die->catch(sub {
-        my($scraper) = b_use('Bivio.Scraper')
-	    ->do_one($list, b_use('Type.DateTime')->now);
+        my($scraper) = $list->get_scraper_class->do_one($list, $_DT->now);
 	$self->internal_put_field(events => $scraper->get('events'));
 	$self->internal_put_field(error => $scraper->get('die')->as_string)
 	    if $scraper->unsafe_get('die');
@@ -61,8 +87,10 @@ sub internal_initialize {
     return $self->merge_initialize_info($self->SUPER::internal_initialize, {
         version => 1,
         visible => [
-	    'Venue.scraper_type',
-	    'Venue.scraper_aux',
+	    'Scraper.scraper_type',
+	    'Scraper.scraper_aux',
+	    'Scraper.default_venue_id',
+	    'Website.url',
 	    {
 		name => 'test_scraper_button',
 		type => 'FormButton',
@@ -70,7 +98,6 @@ sub internal_initialize {
 	    },
 	],
 	other => [
-	    'calendar.Website.url',
 	    {
 		name => 'events',
 		type => 'Array',
@@ -89,12 +116,14 @@ sub internal_pre_execute {
     my($self) = @_;
     $self->internal_put_field(events => []);
     $self->internal_put_field(error => '');
-    $self->internal_put_field('calendar.Website.url' =>
-        $self->new_other('Website')->unauth_load_or_die({
-	    realm_id => $self->req(qw(Model.Venue venue_id)),
-	    location => b_use('Type.Location')->CALENDAR,
-	})->get('url'));;
-    return;
+    return shift->SUPER::internal_pre_execute(@_);
+}
+
+sub _website {
+    my($self) = @_;
+    return $self->new_other('Website')->unauth_load_or_die({
+	realm_id => $self->req(qw(Model.Scraper scraper_id)),
+    });
 }
 
 1;
