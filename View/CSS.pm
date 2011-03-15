5bb50429c0f601d6763bee30725dc02de358777a
Rob Nagler <github@q33.us>
2011-03-15 01:49:52 +0000

    support various ABTest() options.  x5 is current fav

--- View/CSS.pm
+++ View/CSS.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2010-2011 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Cal54::View::CSS;
 use strict;
@@ -6,9 +6,17 @@ use Bivio::Base 'View';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+b_use('Widget.ABTest')->global_init('x5');
 
 sub internal_site_css {
     return shift->SUPER::internal_site_css(@_) . <<'EOF';
+span.b_abtest {
+  display: block;
+  float: right;
+}
+span.b_abtest a {
+  display: block;
+}
 body.c4_home {
   Font('c4_home');
   margin: 2ex 1em;
@@ -22,7 +30,7 @@ div.c4_empty_list {
 table.c4_grid td {
   vertical-align: bottom;
 }
-table.c4_grid td.c4_list {
+table.c4_grid div.c4_list {
   padding-top: 1ex;
   vertical-align: top;
 }
@@ -34,9 +42,6 @@ td.c4_left {
 }
 .c4_query span.label {
   Font('c4_query_label');
-}
-.c4_query input {
-  margin-right: 1em;
 }
 .c4_query .c4_what {
   vertical-align: top;
@@ -53,44 +58,41 @@ td.c4_left {
   padding: 0;
   margin: 0;
 }
-td.c4_right {
-  width: 40em;
-}
-.c4_right div.date {
+.c4_list div.date {
   Font('c4_date');
   margin-bottom: 1ex;
-  border-bottom: 1px solid;
+  border-bottom: 1pt solid;
 }
-.c4_right .item {
+.c4_list .item {
   margin-bottom: 2ex;
 }
-.c4_right .item a {
+.c4_list .item a {
   Font('c4_item_a');
 }
-.c4_right .item a.title {
+.c4_list .item a.title {
   Font('c4_item_a_title');
 }
-.c4_right .item a:hover {
+.c4_list .item a:hover {
   Font('c4_item_a_hover');
 }
-.c4_right .item a:visited {
+.c4_list .item a:visited {
   Font('c4_item_a_visited');
 }
-.c4_right .item div.line {
+.c4_list .item div.line {
   margin-bottom: .2ex;
 }
-.c4_right .item span.time {
+.c4_list .item span.time {
   Font('c4_time');
   margin-right: .2em;
 }
-.c4_right .item .venue {
+.c4_list .item .venue {
   margin-right: .5em;
 }
-.c4_right .item .venue,
-.c4_right .item .address {
+.c4_list .item .venue,
+.c4_list .item .address {
   Font('c4_venue');
 }
-.c4_right .item .excerpt {
+.c4_list .item .excerpt {
   Font('c4_excerpt');
 }
 span.c4_site_name {
@@ -106,8 +108,11 @@ div.c4_sidebar_title {
   Font('c4_sidebar_title');
   margin-bottom: .3ex;
 }
-div.c4_sidebar_list {
-  margin-bottom: 2ex;
+div.c4_sidebar_spacer {
+   margin-bottom: 2ex;
+}
+.c4_cal_input {
+   margin-top: .5ex;
 }
 div.c4_sidebar_list a {
   display: block;
@@ -173,7 +178,402 @@ div.logo_group {
 .increase_font_size {
   float: right;
 }
+#c4_cal_popup {
+  display: none;
+  visibility: hidden;
+}
+.c4_cal_cpDayColumnHeader,
+.c4_cal_cpCurrentMonthDate,
+.c4_cal_cpOtherMonthDate,
+.c4_cal_cpCurrentDate,
+.c4_cal_cpTodayText,
+.c4_cal_cpText {
+  Font('c4_cal');
+}
+.c4_cal_cpYearNavigation,
+.c4_cal_cpMonthNavigation,
+.c4_cal_cpYearNavigation:hover,
+.c4_cal_cpMonthNavigation:hover {
+  text-align: center;
+  vertical-align: middle;
+  Font('c4_cal_month');
+}
+td.c4_cal_cpDayColumnHeader {
+  text-align: right;
+}
+.c4_cal_cpCurrentMonthDate,
+.c4_cal_cpOtherMonthDate,
+.c4_cal_cpCurrentDate {
+  text-align: right;
+  text-decoration: none;
+  Font('c4_cal_disabled');
+}
+.c4_cal_cpCurrentMonthDate,
+.c4_cal_cpCurrentMonthDate:visited {
+  Font('c4_cal_day');
+}
+.c4_cal_cpCurrentMonthDate:hover,
+.c4_cal_cpOtherMonthDate:hover,
+.c4_cal_cpCurrentDate:hover,
+.c4_cal_cpCurrentMonthDate:hover {
+  Font('c4_cal_day_hover');
+}
+.c4_cal_cpCurrentMonthDateDisabled,
+.c4_cal_cpOtherMonthDateDisabled,
+.c4_cal_cpCurrentDateDisabled,
+.c4_cal_cpCurrentMonthDateDisabled:hover,
+.c4_cal_cpOtherMonthDateDisabled:hover,
+.c4_cal_cpCurrentDateDisabled:hover {
+  Font('c4_cal_disabled');
+  text-align: right;
+  text-decoration: line-through;
+}
+.c4_cal_cpCurrentDate,
+.c4_cal_cpCurrentDate:visited {
+  Font('c4_cal_today');
+  text-decoration: underline;
+}
+.c4_cal_cpCurrentDate:hover {
+  Font('c4_cal_today_hover');
+}
+td.c4_cal_cpCurrentDateDisabled
+{
+  border-width: 1px;
+  border: solid thin;
+  Color('c4_cal_disabled-border');
+}
+td.c4_cal_cpTodayText,
+td.c4_cal_cpTodayTextDisabled
+{
+  border: solid 1pt;
+  Color('c4_cal-border');
+  border-width: 1px 0 0 0;
+}
+a.c4_cal_cpTodayText,
+span.c4_cal_cpTodayTextDisabled {
+  height: 1ex;
+}
+td.c4_cal_cpTodayText {
+  text-align: center;
+}
+a.c4_cal_cpTodayText {
+  Font('c4_cal');
+}
+SPAN.c4_cal_cpTodayTextDisabled {
+  Font('c4_cal_disabled');
+}
+.c4_cal_cpBorder {
+  border: solid thin;
+  Color('c4_cal-border');
+}
+td.c4_cal_cpTodayText {
+  display: none;
+  visibility: hidden;
+}
+div.c4_logo_text {
+  vertical-align: top;
+  margin-top: -.5ex;
+}
+div.c4_nav,
+table.c4_grid td {
+  vertical-align: top;
+}
+span.c4_logo_name {
+  display: block;
+  font-size: 250%;
+  font-family: Times;
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: 0;
+}
+span.c4_logo_tag {
+  margin-top: -1.5ex;
+  display: block;
+  text-align: right;
+  font-size: 60%;
+  font-family: Times;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+table.c4_grid .c4_right {
+  text-align: right;
+}
+.c4_query .c4_what {
+  width: 25em;
+}
+table.c4_grid div.c4_list {
+  text-align: left;
+}
+ABTest(
+    x1 => q{
+	div.c4_list {
+	    width: 40em;
+	}
+    },
+    x2 => q{
+	table.c4_grid,
+	div.c4_list {
+	    width: 50em;
+	}
+	span.c4_logo_name,
+	span.c4_logo_tag {
+            color: #0088ce;
+        }
+	div.c4_list .item .venue,
+	div.c4_list .item .address,
+	div.c4_list .item .venue:hover,
+	div.c4_list .item .address:hover,
+	div.c4_list .item a.title,
+	div.c4_list .item a:hover,
+	div.c4_list .item a:visited {
+             color: #0377aa;
+        }
+        div.c4_query input.submit {
+            background-position: center bottom;
+            border: solid thin #cccccc;
+            color: #0;
+            font: 26px Times;
+            padding: 0;
+            height: 36px;
+            width: 4em;
+            margin: 0;
+            vertical-align: top;
+            border-style: solid;
+            color: #0088ce;
+            text-transform: lowercase;
+            background-color: #eeeeee;
+        }
+        div.c4_query .c4_what {
+            height: 26px;
+            padding: 2px 0;
+            padding-right: 10px;
+            margin: 0;
+            color: #0;
+            font: 18px;
+            vertical-align: top;
+        }
+        div.c4_list div.date,
+        div.c4_list .item span.time {
+            color: #303030;
+        }
+    },
+    q{
+	body.c4_home {
+	    margin: 0;
+	}
+        form.c4_form {
+            margin: auto;
+            width: 50em;
+        }
+	.c4_query {
+	    padding-bottom: 0;
+	}
+        table.c4_grid {
+	     width: 100%;
+	     background-color: #0088ce;
+	     margin-bottom: 2ex;
+	}
+	table.c4_grid td.c4_right {
+	     padding-right: 1.5em;
+	     padding-top: 20px;
+	} 
+	table.c4_grid td.c4_left {
+	     padding-left: 1.5em;
+	     padding-top: 16px;
+	     padding-bottom: 1ex;
+	}
+	span.c4_logo_name,
+	span.c4_logo_tag {
+             font-family: arial;
+             color: #ffffff;
+        }
+        span.c4_logo_name {
+             margin-bottom: .2ex;
+             font-size: 48px;
+             margin-right: -.2ex;
+        }
+        span.c4_logo_tag {
+             font-size: 80%;
+        }
+	div.c4_pager a,
+	div.c4_pager a:hover,
+	div.c4_pager a:visited,
+	div.c4_list .item .venue,
+	div.c4_list .item .address,
+	div.c4_list .item .venue:hover,
+	div.c4_list .item .address:hover,
+	div.c4_list .item a.title,
+	div.c4_list .item a:hover,
+	div.c4_list .item a:visited {
+            color: #2200C1;
+        }
+        div.c4_query input.submit {
+            background-position: center bottom;
+            border: solid thin #cccccc;
+            color: #0;
+            font: 18px arial;
+            padding: 0;
+            height: 36px;
+            width: 4em;
+            margin: 0;
+            vertical-align: top;
+            border-style: solid;
+            background-color: #0088ce;
+            border-left: none;
+            color: #ffffff;
+        }
+        div.c4_query .c4_what {
+            height: 26px;
+            padding: 2px 0;
+            margin: 0;
+            color: #0;
+            font: 18px;
+            vertical-align: top;
+        }
+        div.c4_list div.date,
+        div.c4_list .item span.time {
+            color: #303030;
+        }
+        div.c4_list .item span.time {
+            font-weight: normal;
+        }
+	.c4_list .item a.title {
+	    text-decoration: none;
+	}
+	.c4_list .item .venue,
+	.c4_list .item .address {
+	    font-size: 80%;
+	}
+	div.c4_pager {
+	    text-align: center;
+	    padding-top: 2ex;
+	    margin-bottom: 4ex;
+	}
+	div.c4_pager a.next {
+	    margin-left: 1.5em;
+	}
+	div.c4_pager span.c4_month {
+	    margin-right: .5em;
+	    margin-left: 2em;
+	}
+	div.c4_pager a.c4_day {
+	    margin-right: .5em;
+	}
+	span.c4_site_name {
+	    font-family: arial;
+	}
+	div.c4_copy {
+	    margin-bottom: 4ex;
+        }
+        .c4_pager table.c4_month {
+            display: inline;
+            margin: 0 3em;
+        }
+        .c4_pager .c4_month_name {
+            text-align: center;
+            padding: .5ex 0;
+            color: #ffffff;
+            background-color: #0088ce;
+        }
+        .c4_pager td.c4_day {
+            width: 1.5em;
+            text-align: right;
+        }
+        .c4_pager td.c4_day_link a,
+        .c4_pager td.c4_day_link a:hover {
+            color: #2200C1;
+            text-decoration: none;
+            display: block;
+            text-align: right;
+            width: 100%;
+        }
+        .c4_pager td.c4_day_link a:hover {
+            background-color: #0088ce;
+            color: white;
+        }
+        .c4_pager td.c4_day_name {
+            padding-top: .3ex;
+        }
+        .c4_pager td.c4_day_disabled {
+            color: #888888;
+        }
+        span.c4_x5_pager {
+            padding-top: .6ex;
+            padding-left: .8em;
+            display: block;
+            color: white;
+            width: 100%;
+            text-align: left;
+        }
+        .c4_x5_pager .c4_x5_month,
+        .c4_x5_pager .c4_x5_prev,
+        .c4_x5_pager .c4_x5_next {
+            font-size: 80%;
+        }
+        .c4_x5_pager .c4_x5_month {
+            padding-right: .2em;
+            text-transform: uppercase;
+        }
+        .c4_x5_pager .c4_x5_prev {
+            padding-right: 1em;
+        }
+        .c4_x5_pager .c4_x5_spacer,
+        .c4_x5_pager .c4_x5_next {
+            padding-left: 1em;
+        }
+        .c4_x5_pager a {
+            font-size: 80%;
+            padding-left: .3em;
+            color: white;
+        }
+        .c4_x5_pager a.selected {
+            border: 1px solid white;
+            padding-right: .2em;
+            margin-left: .5em;
+        }
+        div.x5_list {
+            padding-top: 13ex;
+            width: 50em;
+            margin: auto;
+        }
+        a.c4_x5_weekend {
+            color: #CCCCCC;
+            font-weight: bold;
+        }
+    },
+);
+ABTest(
+    x4 => q{
+	.c4_list .item a.title {
+	    text-decoration: underline;
+	}
+    },
+    x5 => q{
+        form.c4_form {
+            background-color: white;
+            position: fixed;
+            top: 0;
+            width: 100%;
+            padding-bottom: 3ex;
+        }
+        table.c4_grid {
+            width: 50em;
+            height: 10ex;
+            margin: auto;
+        }
+	span.b_abtest {
+	    padding-top: 13ex;
+	    float: right;
+	}
+    },
+);
 EOF
 }
 
 1;
+
+# Now  Mar [grey]Apr 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ... 30 May Jun
+
+# Clicking moves to that month with a query (not javascript)
+
+# Keep CAL 54 bar sticky.
--- View/Home.pm
+++ View/Home.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 bivio Software, Inc.  All Rights Reserved.
+# Copyright (c) 2010-2011 bivio Software, Inc.  All Rights Reserved.
 # $Id$
 package Cal54::View::Home;
 use strict;
@@ -7,6 +7,7 @@ use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 my($_DT) = b_use('HTMLFormat.DateTime');
+my($_D) = b_use('Type.Date');
 
 #TODO: Deal with not found URIs.  SITE_ROOT must have uri /*, but we don't want to find any page to be this page.
 #TODO: If there is no event page, then render the description in a little popup window
@@ -20,7 +21,10 @@ sub list {
 	    style => RealmCSS(),
 	    body_class => 'c4_home',
 	    head => Title(['CAL 54']),
-	    body => _body(),
+	    body => Join([
+#		ABTest()->choice_links(qw(x2 x3 x4 x5)),
+		_body(),
+	    ]),
 	    xhtml => 1,
 	}),
     );
@@ -52,19 +56,71 @@ sub pre_compile {
 }
 
 sub _body {
-    return Grid(
-	[
-	    [
-		_logo()->put(cell_class => 'c4_left'),
-		_form()->put(cell_class => 'c4_right'),
-	    ],
-	    [
-		_sidebar()
-		    ->put(cell_class => 'c4_left c4_list'),
-		_list()->put(cell_class => 'c4_right c4_list'),
-	    ],
-	],
-	{class => 'c4_grid'},
+    return ABTest(
+	x5 => Join([
+	    Form({
+		form_class => 'HomeQueryForm',
+		class => 'c4_form',
+		form_method => 'get',
+		want_hidden_fields => 0,
+		value => Grid([[
+		    _logo()->put(cell_class => 'c4_left'),
+		    _form()->put(cell_class => 'c4_right'),
+		]], {
+		    class => 'c4_grid',
+		}),
+	    }),
+	    DIV_x5_list(_list()),
+	]),
+	Form({
+	    form_class => 'HomeQueryForm',
+	    class => 'c4_form',
+	    form_method => 'get',
+	    want_hidden_fields => 0,
+	    value => ABTest(
+		x1 => Grid(
+		    [
+			[
+			    _logo()->put(cell_class => 'c4_left'),
+			    _form()->put(cell_class => 'c4_right'),
+			],
+			[
+			    _sidebar()
+				->put(cell_class => 'c4_left c4_list'),
+			    _list()->put(
+				cell_class => 'c4_right c4_list',
+				cell_colspan => 2,
+			    ),
+			],
+		    ],
+		    {class => 'c4_grid'},
+		),
+		x2 => Grid(
+		    [
+			[
+			    _logo()->put(cell_class => 'c4_left'),
+			    _form()->put(cell_class => 'c4_right'),
+			],
+			[
+			    _list()->put(
+				cell_class => 'c4_right c4_list',
+				cell_colspan => 2,
+			    ),
+			],
+		    ],
+		    {class => 'c4_grid'},
+		),
+		Join([
+		    Grid([[
+			_logo()->put(cell_class => 'c4_left'),
+			_form()->put(cell_class => 'c4_right'),
+		    ]], {
+			class => 'c4_grid',
+		    }),
+		    _list(),
+		]),
+	    ),
+	}),
     );
 }
 
@@ -76,21 +132,19 @@ sub _body_mobile {
 }
 
 sub _form {
-    return Form({
-	form_class => 'HomeQueryForm',
-	form_method => 'get',
-	class => 'c4_query',
-	want_hidden_fields => 0,
-	value => Join([
+    return DIV_c4_query(
+	Join([
 	    Hidden('when'),
+	    ABTest()->hidden_form_field(),
 	    Text('what', {class => 'c4_what', size => 50}),
 	    INPUT({
 		TYPE => 'submit',
 		VALUE => 'Search',
 		class => 'submit',
 	    }),
+	    ABTest(x5 => [\&_x5_pager]),
 	]),
-    });
+    );
 }
 
 sub _form_mobile {
@@ -165,7 +219,7 @@ sub _form_mobile {
 
 sub _list {
     return Join([
-	List(HomeList => [
+	DIV_c4_list(List(HomeList => [
 	    DIV_date(['month_day']),
 	    DIV_item(Join([
 		DIV_line(Join([
@@ -177,27 +231,61 @@ sub _list {
 			'title',
 		    ),
 		])),
-		DIV_line(Join([
-		    Link(
-			String(['venue.RealmOwner.display_name']),
-			['Website.url'],
-			'venue',
-		    ),
-		    ' ',
-		    Link(
-			String(['address']),
-			['map_uri'],
-			'address',
-		    ),
-		])),
-		DIV_excerpt(String(['excerpt'])),
+		ABTest(
+		    qr{x1|x2} => Join([
+			DIV_line(Join([
+			    Link(
+				String(['owner.RealmOwner.display_name']),
+				['Website.url'],
+				'venue',
+			    ),
+			    ' ',
+			    Link(
+				String(['address']),
+				['map_uri'],
+				'address',
+			    ),
+			])),
+			DIV_excerpt(String(['excerpt'])),
+		    ]),
+		    Join([
+			DIV_excerpt(String(['excerpt'])),
+			DIV_line(Join([
+			    Link(
+				String(['venue.RealmOwner.display_name']),
+				['Website.url'],
+				'venue',
+			    ),
+			    ' ',
+			    Link(
+				String(['address']),
+				['map_uri'],
+				'address',
+			    ),
+			])),
+		    ]),
+		),
 	    ])),
 	], {
 	    empty_list_widget => DIV_c4_empty_list(
 		q{Your search didn't match any results.  Try a different query.},
 	    ),
-	    class => 'c4_list',
-	}),
+	})),
+	ABTest(
+	    x3 => DIV_c4_pager(Join([
+		Link('<< Earlier', '#', 'back'),
+		SPAN_c4_month('March'),
+		map(Link($_, '#', 'c4_day'), qw(29 30 31)),
+		SPAN_c4_month('April'),
+		map(Link($_, '#', 'c4_day'), qw(1 2 3 4)),
+		Link('Later >>', '#', 'next'),
+	    ])),
+	    x4 => DIV_c4_pager(Join([
+		_month(3),
+		_month(4),
+		_month(5),
+	    ])),
+	),
 	DIV_c4_copy(Prose(
 	    "&copy; @{[__PACKAGE__->use('Type.DateTime')->now_as_year]} SPAN_c4_site_name('CAL 54&trade;'); Boulder's Calendar&trade;")),
     ]);
@@ -249,25 +337,139 @@ sub _list_mobile {
 }
 
 sub _logo {
-    return Image({
-    	src => 'logo',
-    	alt_text => 'CAL 54',
-    	class => 'c4_logo',
-    });
+    return ABTest(
+	x1 => Image({
+	    src => 'logo',
+	    alt_text => 'CAL 54',
+	    class => 'c4_logo',
+	}),
+	x2 => DIV_c4_logo_text(
+	    Join([
+		SPAN_c4_logo_name('CAL 54'),
+		SPAN_c4_logo_tag(q{Boulder's Calendar}),
+	    ]),
+	),
+	DIV_c4_logo_text(
+	    Join([
+		SPAN_c4_logo_name('CAL 54'),
+		SPAN_c4_logo_tag(q{Boulder's Calendar}),
+	    ]),
+	),
+    );
+}
+
+sub _month {
+    my($which) = @_;
+    my($now) = $_D->local_today;
+    my($d) = $_D->date_from_parts(1, $which, 2011);
+    my($name) = $_D->english_month($_D->get_parts($d, 'month'));
+    my($bom) = $d;
+    my($eom) = $_D->set_end_of_month($bom);
+    $d = $_D->set_beginning_of_week($bom);
+    my($eow) = $_D->set_end_of_week($eom);
+    my($done) = 0;
+    return Grid(
+	[
+	    [
+		String(
+		    $name,
+		    {
+			cell_class => 'c4_month_name',
+			cell_colspan => 7,
+		    },
+		),
+	    ],
+	    [
+		map(
+		    String($_, {cell_class => 'c4_day_name c4_day'}),
+		    qw(S M T W T F S),
+		),
+	    ],
+	    map(
+		$done ? () : [
+		    map(
+			{
+			    my($res) = ' ';
+			    my($class) = 'c4_day_none';
+			    unless ($done || $_D->is_less_than($d, $bom)) {
+				if ($_D->is_greater_than($d, $eom)) {
+				    $done = 1;
+				}
+				else {
+				    $res = $_D->get_parts($d, 'day');
+				    $class = $_D->is_less_than($d, $now)
+					? 'c4_day_disabled'
+					: 'c4_day_link';
+				}
+			    }
+			    $d = $_D->add_days($d, 1);
+			    $class eq 'c4_day_link'
+				? Link(
+				    String($res),
+				    URI({
+					query => {
+					    when => $_D->to_string($d),
+					    what => [qw(->ureq query what)],
+					},
+				    }),
+				    {cell_class => "$class c4_day"},
+				)
+				: String(
+				    $res,
+				    {
+					cell_class => "$class c4_day",
+					hard_spaces => 1,
+				    },
+				);
+			}
+			1 .. 7,
+		    ),
+		],
+		1 .. 6,
+	    ),
+	],
+	{class => 'c4_month'},
+    ),
 }
 
 sub _sidebar {
     return Join([
 	DIV_c4_sidebar_title(String('When')),
-	DIV_c4_sidebar_list(List(
-	    'WhenList',
-	    [Link(
-		['item'],
-		URI({
-		    query => ['->search_query'],
-		}),
-	    )],
-	)),
+	Join([
+	    DIV_c4_sidebar_list(List(
+		'WhenList',
+		[Link(
+		    ['item'],
+		    URI({
+			query => ['->search_query'],
+		    }),
+		)],
+	    )),
+	    SCRIPT({
+		TYPE => 'text/javascript',
+		SRC => '/b/mattkruse/CalendarPopup.js',
+		tag_if_empty => 1,
+		value => '',
+	    }),
+	    SCRIPT({
+		TYPE => 'text/javascript',
+		value => q{var cal1x = new CalendarPopup("c4_cal_popup"); cal1x.setCssPrefix("c4_cal_"); var now = new Date();(now.setDate(now.getDate() - 1)); cal1x.addDisabledDates(null, now.toLocaleDateString());},
+	    }),
+	    INPUT({
+		TYPE => 'text',
+		NAME => 'date1x',
+		VALUE => '',
+		SIZE => '12',
+		class => 'c4_cal_input',
+	    }),
+	    Simple(q{<A HREF="#" onClick="cal1x.select(document.forms[0].date1x,'anchor1x','MM/dd/yyyy'); return false;" NAME="anchor1x" ID="anchor1x">&#9660;</A>}),
+	    DIV({
+		tag_if_empty => 1,
+		value => '',
+		id => 'c4_cal_popup',
+	    }),
+	    DIV_c4_sidebar_spacer(''),
+	]),
 	DIV_c4_sidebar_title(String('Popular Searches')),
 	DIV_c4_sidebar_list(List(
 	    'PopularList',
@@ -279,6 +481,94 @@ sub _sidebar {
 	    )],
 	)),
     ]);
+}
+
+sub _when_uri {
+    my($date) = @_;
+    $date = $_D->to_string($date);
+    return URI({
+	query => {
+	    when => $date,
+	    what => Join([
+		[
+		    sub {
+			my(undef, $what) = @_;
+			$what = ''
+			    unless defined($what);
+			$what =~ s{(\d+/\d+(?:/\d+)?)\s+}{};
+			return $what;
+		    },
+		    [qw(->ureq query what)],
+		],
+	    ]),
+	},
+    });
+}
+
+sub _x5_pager {
+    my($source) = @_;
+    my($now) = $_D->local_today;
+    my($when) = ($_D->from_literal($source->ureq(qw(query when))))[0]
+	|| $now;
+    my($start) = $_D->set_beginning_of_week($when);
+    my($first) = 1;
+    return SPAN_c4_x5_pager(
+	Join([
+	    $_D->is_less_than($start, $now) ? ()
+		: Link(
+		    '<<',
+		    _when_uri($_D->add_days($start, -21)),
+		    'c4_x5_prev',
+		),
+	    map(
+		{
+		    my($week) = $_;
+		    (
+			$first ? () : SPAN_c4_x5_spacer(''),
+			map(
+			    {
+				my($d) = $_D->add_days($start, $week * 7 + $_);
+				my($selected) = $_D->is_equal($d, $when) ? ' selected' : '';
+				my($day) = $_D->get_parts($d, 'day');
+				my($wday) = $_D->english_day_of_week($d);
+				my($weekend) = $_D->english_day_of_week($d) =~ /^s/i
+				    ? ' c4_x5_weekend' : ''; 
+				my($month) = [];
+				if ($first || $day == 1) {
+				    $month = [
+					$first ? () : SPAN_c4_x5_spacer(''),
+					SPAN_c4_x5_month(
+					    $_D->english_month3($_D->get_parts($d, 'month')),
+					),
+				    ];
+				    $first = 0;
+				}
+				(
+				    @$month,
+				    $_D->is_less_than($d, $now) ? ()
+					: Link(
+					    $day,
+					    _when_uri($d),
+					    {
+						class => "c4_x5_day$selected$weekend",
+						TITLE => $_D->english_day_of_week($d),
+					    },
+					),
+				);
+			    }
+			    0 .. 6,
+			),
+		    );
+		}
+		0 .. 2,
+	    ),
+	    Link(
+		'>>',
+		_when_uri($_D->add_days($start, +21)),
+		'c4_x5_next',
+	    ),
+	]),
+    );
 }
 
 1;
