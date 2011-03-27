45232b2472b825cc30d6ad85228c5ba13945ab98
Rob Nagler <github@q33.us>
2011-03-27 18:20:06 +0000

    *** empty log message ***

---
+++ Facade/Mobile.pm
@@ -0,0 +1,31 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Cal54::Facade::Mobile;
+use strict;
+use Bivio::Base 'Facade.Cal54';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+__PACKAGE__->new({
+    clone => 'Cal54',
+    is_production => 1,
+    http_host => 'm.cal54.com',
+    mail_host => 'm.cal54.com',
+    uri => 'm.cal54',
+    CSS => [
+	[c4_query_what => 'width: 15em;'],
+	[c4_form => ''],
+	[c4_list => ''],
+	[c4_home_bottom_pager => ''],
+    ],
+    Font => [
+	[c4_home => ['family=Arial, Helvetica, sans-serif', '120%']],
+	[[qw(c4_logo_tag c4_logo_name)] => [qw(bold uppercase 80%)]],
+	[c4_query_what => []],
+	[c4_query_submit => []],
+	[c4_pager_month => ['65%', 'uppercase']],
+	[c4_pager_a => ['65%']],
+    ],
+});
+
+1;
