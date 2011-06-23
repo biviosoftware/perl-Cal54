0064629bc915a897336a02981caf33374acf4fc7
Justin Schell <schellj@bivio.biz>
2011-06-23 18:37:42 +0000

    hack in featured event

--- View/Home.pm
+++ View/Home.pm
@@ -67,46 +67,87 @@ sub _list {
 		},
 	    ),
 	),
-	DIV_c4_list(List(HomeList => [
-	    DIV_date(['month_day']),
-	    DIV_item(Join([
-		DIV_line(Join([
-		    UserTrackingLink(
-			String(['RealmOwner.display_name']),
-			Or(['CalendarEvent.url'], ['calendar.Website.url']),
-			'title',
-		    ),
-		])),
-		Join([
-		    SPAN_time(String(['start_end_am_pm'])),
-		    ' ',
-		    SPAN_excerpt(String(['excerpt'])),
-		    DIV_line(Join([
-			UserTrackingLink(
-			    String(['venue.RealmOwner.display_name']),
-			    ['Website.url'],
-			    'venue',
-			),
-			' ',
-			UserTrackingLink(
-			    String(['address']),
-			    ['map_uri'],
-			    'address',
-			),
-			If(['Phone.phone'],
-			   Join([
-			       ' ',
-			       SPAN_phone(['Phone.phone']),
-			   ]),
-		       ),
+	DIV_c4_list(
+	    Grid([[
+		List(HomeList => [
+		    DIV_date(['month_day']),
+		    DIV_item(Join([
+			DIV_line(Join([
+			    UserTrackingLink(
+				String(['RealmOwner.display_name']),
+				Or(['CalendarEvent.url'], ['calendar.Website.url']),
+				'title',
+			    ),
+			])),
+			Join([
+			    SPAN_time(String(['start_end_am_pm'])),
+			    ' ',
+			    SPAN_excerpt(String(['excerpt'])),
+			    DIV_line(Join([
+				UserTrackingLink(
+				    String(['venue.RealmOwner.display_name']),
+				    ['Website.url'],
+				    'venue',
+				),
+				' ',
+				UserTrackingLink(
+				    String(['address']),
+				    ['map_uri'],
+				    'address',
+				),
+				If (['Phone.phone'],
+				    Join([
+					' ',
+					SPAN_phone(['Phone.phone']),
+				    ]),
+				),
+			    ])),
+			]),
 		    ])),
-		]),
-	    ])),
-	], {
-	    empty_list_widget => DIV_c4_empty_list(
-		q{Your search didn't match any results.  Try a different query.},
-	    ),
-	})),
+		], {
+		    empty_list_widget => DIV_c4_empty_list(
+			q{Your search didn't match any results.  Try a different query.},
+		    ),
+		}),
+		DIV_featured(
+		    Join([
+			SPAN_title('Featured'),
+			DIV_date(q{Sunday June 26, 2011}),
+			DIV_item(Join([
+			    DIV_line(
+				UserTrackingLink(
+				    'Riff Raff - RDS Foundation Fundraiser',
+				    'http://rdsfoundation.org/index.php?option=com_content&task=view&id=32',
+				    'title',
+				),
+			    ),
+			    Join([
+				SPAN_time(q{6pm - 9}),
+				' ',
+				SPAN_excerpt(
+				    q{Boulder's Riff Raff will be playing for the fundraising dinner of the RDS Foundation (www.rdsfoundation.org - we raise money for people with bipolar disorder to get the treatment they need and can't afford).  Tickets for this outdoor event are $50 which include the concert and a catered full dinner and wine from Laudisio's.}
+				),
+				DIV_line(Join([
+				    UserTrackingLink(
+					q{BC Interiors},
+					'http://www.bcinteriors.com/',
+					'venue',
+				    ),
+				    ' ',
+				    UserTrackingLink(
+					'3390 Valmont Rd, Boulder',
+					'http://maps.google.com/maps?q=BC+Interiors+3390+Valmont+Rd,+Boulder+CO,+80301',
+					'address',
+				    ),
+				    ' ',
+				    SPAN_phone('(303) 555-5555'),
+				])),
+			    ]),
+			])),
+		    ]),
+		),
+	    ]]),
+	),
 	IfMobile(
 	    Link(
 		String(vs_text('next_button')),
