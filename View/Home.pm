8603a42a573e39e6ea3894a6243c5cb39a83fdfb
Paul Moeller <moeller@bivio.biz>
2011-06-24 19:54:46 +0000

    moved feature event to separate method, only show if not mobile

--- View/Home.pm
+++ View/Home.pm
@@ -39,6 +39,44 @@ sub list {
     ]));
 }
 
+sub _featured_item {
+    return DIV_featured(Join([
+	SPAN_title('Featured Event'),
+	DIV_date(q{Sunday June 26, 2011}),
+	DIV_item(Join([
+	    DIV_line(
+		UserTrackingLink(
+		    'Riff Raff - RDS Foundation Fundraiser',
+		    'http://rdsfoundation.org/index.php?option=com_content&task=view&id=32',
+		    'title',
+		),
+	    ),
+	    Join([
+		SPAN_time(q{6pm - 9}),
+		' ',
+		SPAN_excerpt(
+		    q{Boulder's Riff Raff will be playing for the fundraising dinner of the RDS Foundation (www.rdsfoundation.org - we raise money for people with bipolar disorder to get the treatment they need and can't afford).  Tickets for this outdoor event are $50 which include the concert and a catered full dinner and wine from Laudisio's.}
+		),
+		DIV_line(Join([
+		    UserTrackingLink(
+			q{BC Interiors},
+			'http://www.bcinteriors.com/',
+			'venue',
+		    ),
+		    ' ',
+		    UserTrackingLink(
+			'3390 Valmont Rd, Boulder',
+			'http://maps.google.com/maps?q=BC+Interiors+3390+Valmont+Rd,+Boulder+CO,+80301',
+			'address',
+		    ),
+		    ' ',
+		    SPAN_phone('303.555.5555'),
+		])),
+	    ]),
+	])),
+    ]));
+}
+
 sub _form {
     return DIV_c4_query(
 	Join([
@@ -109,43 +147,10 @@ sub _list {
 			q{Your search didn't match any results.  Try a different query.},
 		    ),
 		}),
-		DIV_featured(
-		    Join([
-			SPAN_title('Featured Event'),
-			DIV_date(q{Sunday June 26, 2011}),
-			DIV_item(Join([
-			    DIV_line(
-				UserTrackingLink(
-				    'Riff Raff - RDS Foundation Fundraiser',
-				    'http://rdsfoundation.org/index.php?option=com_content&task=view&id=32',
-				    'title',
-				),
-			    ),
-			    Join([
-				SPAN_time(q{6pm - 9}),
-				' ',
-				SPAN_excerpt(
-				    q{Boulder's Riff Raff will be playing for the fundraising dinner of the RDS Foundation (www.rdsfoundation.org - we raise money for people with bipolar disorder to get the treatment they need and can't afford).  Tickets for this outdoor event are $50 which include the concert and a catered full dinner and wine from Laudisio's.}
-				),
-				DIV_line(Join([
-				    UserTrackingLink(
-					q{BC Interiors},
-					'http://www.bcinteriors.com/',
-					'venue',
-				    ),
-				    ' ',
-				    UserTrackingLink(
-					'3390 Valmont Rd, Boulder',
-					'http://maps.google.com/maps?q=BC+Interiors+3390+Valmont+Rd,+Boulder+CO,+80301',
-					'address',
-				    ),
-				    ' ',
-				    SPAN_phone('(303) 555-5555'),
-				])),
-			    ]),
-			])),
-		    ]),
-		),
+		IfMobile(
+		    Simple(''),
+		    _featured_item(),
+		)->put(cell_align => 'top'),
 	    ]]),
 	),
 	IfMobile(
