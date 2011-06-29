a2e53e26fc194d1bd4acc27f43d761558428ee58
Paul Moeller <moeller@bivio.biz>
2011-06-29 16:51:18 +0000

    added support for displaying RSS data

--- Action/ScraperPreview.pm
+++ Action/ScraperPreview.pm
@@ -11,14 +11,39 @@ sub execute {
     $self->put(text => '');
     my($uri) = ($self->req('query') || {})->{x};
     return unless $uri;
-    my($scraper) = b_use('HTML.Scraper')->new;
     my($cleaner) = b_use('Bivio.HTMLCleaner')->new;
     $self->put(
 	cleaner => $cleaner,
-	text => ${$cleaner->clean_html(
-	    $scraper->extract_content($scraper->http_get($uri)), $uri)},
+	text => _parse_content($self, $uri,
+	    $cleaner),
     );
     return;
+}
+
+sub _parse_content {
+    my($self, $uri, $cleaner) = @_;
+    my($scraper) = b_use('HTML.Scraper')->new;
+    my($content) = $scraper->extract_content($scraper->http_get($uri));
+
+    # rss
+    if ($$content =~ /^\s*\<\?xml version/) {
+	my($xml) = b_use('Bivio.Scraper')->parse_xml($content);
+	my($items) = [];
+
+	foreach my $item (@{$xml->{channel}->{item}}) {
+	    $item->{description} = ${$cleaner->clean_html(
+	        \($item->{description}),
+		$item->{link},
+	    )} if $item->{description} =~ /\<.*\>/;
+	    push(@$items, {
+		description => $item->{description},
+		summary => $item->{title},
+		url => $item->{link},
+	    });
+	}
+	return ${b_use('IO.Ref')->to_string($items)};
+    }
+    return ${$cleaner->clean_html($content, $uri)};
 }
 
 1;
