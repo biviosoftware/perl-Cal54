df47f58c1220d7ee671da44919d4832e9543578c
Paul Moeller <moeller@bivio.biz>
2011-10-04 16:16:57 +0000

    initial revision

---
+++ Scraper/AEGLive.pm
@@ -0,0 +1,33 @@
+# Copyright (c) 2011 bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Cal54::Scraper::AEGLive;
+use strict;
+use Bivio::Base 'Scraper.RegExp';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub eval_scraper_aux {
+    my($self) = @_;
+    return shift->SUPER::eval_scraper_aux(<<'EOF');
+{
+  pager => {
+    link => qr/(Next >>.*\})/i,
+    page_count => 3,
+  },
+  repeat => [
+    [qr/\n(.*?\}\n(?:.*\n)?)$month $day, $year, $time_ap/ => {
+      fields => [qw(summary month day year start_time)],
+      follow_link => {
+        once => [
+          [qr/Ticket Prices.*?\n(?:[^\n]{0,50}\n)+(.*?)Service and handling fees/is => {
+            fields => [qw(description)],
+          }],
+        ],
+      },
+    }],
+  ],
+}
+EOF
+}
+
+1;
