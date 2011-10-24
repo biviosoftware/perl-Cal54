e62db526f54870251afeb435a7efc06c695058cb
Rob Nagler <github@q33.us>
2011-10-24 03:24:34 +0000

    removed execute() - subclass EmptyReply made simpler by combining conditions more b_use use UserAgent->is_actual_browser

--- Action/UserTracking.pm
+++ Action/UserTracking.pm
@@ -2,64 +2,58 @@
 # $Id$
 package Cal54::Action::UserTracking;
 use strict;
-use Bivio::Base 'Biz.Action';
+use Bivio::Base 'Action.EmptyReply';
 b_use('IO.Trace');
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 our($_TRACE);
 my($_ENTRY_URI) = 'c4e';
 my($_REF_URI) = 'c4r';
+my($_UCF) = b_use('Model.UserCreateForm');
+my($_R) = b_use('Biz.Random');
+my($_UA) = b_use('Type.UserAgent');
 my($_ULF) = b_use('Model.UserLoginForm');
+# TaskLog must register as a handler first
+my($_TL) = b_use('Model.TaskLog');
 b_use('Agent.Task')->register(__PACKAGE__);
-
-sub execute {
-    my($proto, $req) = @_;
-    $req->get('reply')->set_output(\('ok'));
-    return 1;
-}
 
 sub handle_pre_execute_task {
     my($proto, undef, $req) = @_;
-    my($cookie) = $req->ureq('cookie');
-    return unless $cookie;
-    return if $cookie->unsafe_get($_ULF->USER_FIELD);
-
-    if ($req->unsafe_get('Type.UserAgent')) {
-	return
-	    unless $req->get('Type.UserAgent')->is_browser;
-	return
-	    if $req->get('Type.UserAgent')->eq_browser_robot;
-    }
+    return
+	if !(my $cookie = $req->ureq('cookie'));
+    return
+	if $cookie->unsafe_get($_ULF->USER_FIELD)
+	|| !$_UA->is_actual_browser($req);
     my($self) = $proto->new($req);
-
     if ($cookie->unsafe_get($_ENTRY_URI)) {
 	_trace('clearing cookie, creating user') if $_TRACE;
 	_create_user($self, $cookie);
+	return;
     }
-    else {
-	my($r) = $self->ureq('r');
-	return unless $r;
-	_trace('adding cookie: ', $r->header_in('Referer'), ' ', $r->uri)
-	    if $_TRACE;
-	$cookie->put($_REF_URI => $r->header_in('Referer') || '');
-	$cookie->put($_ENTRY_URI => $r->uri);
-    }
+    return
+	unless my $r = $self->ureq('r');
+    _trace('adding cookie: ', $r->header_in('Referer'), ' ', $r->uri)
+	if $_TRACE;
+    $cookie->put(
+	$_REF_URI => $r->header_in('Referer') || '',
+	$_ENTRY_URI => $r->uri,
+    );
     return;
 }
 
 sub _create_user {
     my($self, $cookie) = @_;
-    b_use('Model.UserCreateForm')->execute($self->req, {
+    my($req) = $self->req;
+    $_UCF->execute($req, {
 	'RealmOwner.display_name' => 'x',
-	'RealmOwner.password' => b_use('Biz.Random')->string(8),
+	'RealmOwner.password' => $_R->string(8),
 	without_login => 1,
     });
-    my($user) = $self->req('Model.User');
+    my($user) = $req->get('Model.User');
     $user->update({
 	last_name => 'u' . $user->get('user_id'),
     });
-    $self->req->set_realm($user->get('user_id'));
-
+    $req->set_realm($user->get('user_id'));
     foreach my $info (
 	[C4_USER_ENTRY_URI => $_ENTRY_URI],
 	[C4_USER_REFERER_URI => $_REF_URI],
@@ -70,8 +64,8 @@ sub _create_user {
 	$cookie->delete($field);
     }
     $cookie->put($_ULF->USER_FIELD => $user->get('user_id'));
-    $_ULF->handle_cookie_in($cookie, $self->req);
-    b_use('Model.TaskLog')->set_user_id($self->req, $user->get('user_id'));
+    $_ULF->handle_cookie_in($cookie, $req);
+    $_TL->set_user_id($req, $user->get('user_id'));
     return;
 }
 
