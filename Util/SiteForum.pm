81454e26d8cf6fe23b4a4d41555648480757ae4a
Rob Nagler <github@q33.us>
2012-03-04 22:18:08 +0000

    fpc

--- Util/SiteForum.pm
+++ Util/SiteForum.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 CAL54, Inc.  All Rights Reserved.
+# Copyright (c) 2011-2012 CAL54, Inc.  All Rights Reserved.
 # $Id$
 package Cal54::Util::SiteForum;
 use strict;
@@ -11,11 +11,11 @@ sub init_realms {
     my(@res) = shift->SUPER::init_realms(@_);
     my($req) = $self->initialize_fully;
     $self->model('EmailAlias')->create({
-	$req->format_email('external', $req),
+	incoming => $req->format_email('external', $req),
     	outgoing => $self->CONTACT_REALM,
     });
     $self->model('EmailAlias')->create({
-	$req->format_email('jmrukkers', $req),
+	incoming => $req->format_email('jmrukkers', $req),
     	outgoing => 'jmrukkers@gmail.com',
     });
     return @res;
