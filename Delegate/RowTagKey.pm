69d57b0354d4adf572117773886785ed7dd5ea3e
Paul Moeller <moeller@bivio.biz>
2013-04-19 22:16:24 +0000

    added internal_get_type()

--- Delegate/RowTagKey.pm
+++ Delegate/RowTagKey.pm
@@ -5,16 +5,36 @@ use strict;
 use Bivio::Base 'Delegate';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_INFO, $_TYPE) = _init();
 
 sub get_delegate_info {
     my($proto) = @_;
     return [
 	@{$proto->SUPER::get_delegate_info},
-	C4_HIDDEN_CALENDAR_EVENT => [100, 'Boolean'],
-	C4_USER_ENTRY_URI => [101, 'Text64K'],
-	C4_USER_REFERER_URI => [102, 'Text64K'],
-	C4_MOST_RECENT_SEARCH => [103, 'Line'],
+	@$_INFO,
     ];
+}
+
+sub internal_get_type {
+    my($self) = @_;
+    return $_TYPE->{$self->get_name} || shift->SUPER::internal_get_type(@_);
+}
+
+sub _init {
+    my($type) = {};
+    return (
+	[map(
+	    {
+		$type->{$_->[0]} = $_->[2];
+		($_->[0], $_->[1]);
+	    }
+	    [C4_HIDDEN_CALENDAR_EVENT => 100, 'Boolean'],
+	    [C4_USER_ENTRY_URI => 101, 'Text64K'],
+	    [C4_USER_REFERER_URI => 102, 'Text64K'],
+	    [C4_MOST_RECENT_SEARCH => 103, 'Line'],
+	)],
+	$type,
+    );
 }
 
 1;
