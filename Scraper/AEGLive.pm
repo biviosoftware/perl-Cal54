e6840343dd526b981af26f4cb24ab44eef76d836
Paul Moeller <moeller@bivio.biz>
2013-08-14 22:51:35 +0000

    new def

--- Scraper/AEGLive.pm
+++ Scraper/AEGLive.pm
@@ -12,15 +12,21 @@ sub eval_scraper_aux {
 {
   crawl_delay => 5,
   pager => {
-    link => qr/(Next >>.*\})/i,
+    link => qr/\n(>.*\})\n/i,
     page_count => 3,
   },
   repeat => [
-    [qr/\n(.*?\}\n(?:.*\n)?)$month $day, $year, $time_ap/ => {
-      fields => [qw(summary month day year start_time)],
+    [qr/\n(http.*?)\n+$day_name, $month $day, $year $time_ap/ => {
+      fields => [qw(link day_name month day year start_time)],
       follow_link => {
         once => [
+          [qr/\nEvent Details\n+(.*?)\n\n/is => {
+            fields => [qw(summary)],
+          }],
           [qr/Ticket Prices.*?\n(?:[^\n]{0,50}\n)+(.*?)Service and handling fees/is => {
+            fields => [qw(description)],
+          }],
+          [qr/Ticket Prices.*?\nWebsites\n(?:[^\n]{0,50}\n)+(.*?)Service and handling fees/is => {
             fields => [qw(description)],
           }],
         ],
