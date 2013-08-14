bd8ee363b2bfc024d69e18ab5e8e4c03f6c5777a
Paul Moeller <moeller@bivio.biz>
2013-08-14 22:51:20 +0000

    initial revision

---
+++ Scraper/Defunct.pm
@@ -0,0 +1,13 @@
+# Copyright (c) 2013 Bivio Software, Inc.  All Rights Reserved.
+# $Id$
+package Cal54::Scraper::Defunct;
+use strict;
+use Bivio::Base 'Scraper.RegExp';
+
+our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+
+sub internal_import {
+    return;
+}
+
+1;
