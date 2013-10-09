584988e3f61f0a299b8ba94caf7c0954d5993d2b
Paul Moeller <moeller@bivio.biz>
2013-10-09 15:22:11 +0000

    changed copyright

--- Facade/Cal54Base.pm
+++ Facade/Cal54Base.pm
@@ -9,7 +9,7 @@ our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
 sub internal_c4_text {
     return [
 	[site_name => q{CAL54}],
-	[site_copyright => q{CAL54, Inc.}],
+	[site_copyright => q{bivio Software, Inc.}],
 	[c4_home_title => q{Calendar of Events, Concerts, Lectures, Activities for Boulder/Denver}],
 	[c4_home_list_title => q{The Searchable Event Calendar for Boulder and Denver}],
     ];
