d23a9f74e1094bb1534b87d9ee42d0d8b113392c
Paul Moeller <moeller@bivio.biz>
2014-07-02 19:17:21 +0000

    moved defaults to Cal54Base

--- Facade/Cal54.pm
+++ Facade/Cal54.pm
@@ -5,6 +5,7 @@ use strict;
 use Bivio::Base 'Facade.Cal54Base';
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
+my($_DT) = b_use('Type.DateTime');
 
 my($_SELF) = __PACKAGE__->new({
     uri => 'cal54',
@@ -16,12 +17,9 @@ my($_SELF) = __PACKAGE__->new({
 	[[qw(c4_item_a c4_item_a_hover b_mobile_toggler_a)] => 0x0077b3],
         [c4_item_a_visited => 0x591899],
 	[[qw(c4_date c4_time c4_query_submit_background c4_query_background c4_grid_background c4_home_title_background c4_featured_background c4_button_background c4_home_list_title)] => 0xc82127],
-	[[qw(c4_pager c4_pager_selected_border c4_form_background c4_logo_name c4_logo_tag c4_home_title c4_featured c4_button c4_query_submit)] => 0xffffff],
-	[[qw(c4_pager_weekend c4_query_submit_border c4_query_submit)] => 0xf8f8f8],
-	[[qw(c4_pager_a)]=> 0xe8e8e8],
     ],
     Constant => [
-	[ActionError_default_view => 'HomeOther->error_default'],
+	[ActionError_default_view => 'Home->error_default'],
 	[ActionError_want_wiki_view => 0],
 	[xlink_c4_about => {
 	    task_id => 'C4_HOME_WIKI_VIEW',
@@ -29,77 +27,20 @@ my($_SELF) = __PACKAGE__->new({
 	}],
     ],
     CSS => [
-	[c4_query_what => 'width: 25em;'],
-        [c4_form => q{IfUserAgent(
-            '!is_msie_6_or_before',
-            '
-                position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-            ',
-        );}],
-	[c4_grid => q{
-	    width: 50em;
-	    margin: auto;
-	}],
-	[c4_list => q{IfUserAgent(
-		'!is_msie_6_or_before',
-		'padding-top: 13ex;',
-	    );
-	    width: 49.5em;
-	    padding-left: .5em;
-	    margin: auto;
-        }],
-	[c4_home_bottom_pager => q{
-            width: 50em;
-            margin: auto;
-        }],
-	[c4_home_other => q{
-	    width: 49.5em;
-	    padding-left: .5em;
-	    margin: auto;
-	    text-align: center;
-        }],
+	@{__PACKAGE__->internal_c4_css},
+	[html_body => 'padding-top: 0;'],
     ],
     Font => [
+	@{__PACKAGE__->internal_c4_font},
 	[body => []],
 	[b_mobile_toggler_selected => ['normal_weight']],
 	[b_mobile_toggler_a => ['normal_weight']],
-	[c4_home => ['family=Arial, Helvetica, sans-serif', 'medium']],
-	[c4_query_submit => ['size=18px']],
-	[c4_logo_name => [qw(uppercase bold 48px)]],
-	[c4_home_title => [qw(uppercase bold 200%)]],
-	[c4_excerpt => ['80%']],
-	[c4_query_what => '120%'],
-	[c4_pager_weekend => 'bold'],
-	[c4_time => [qw(80% bold)]],
-	[c4_date => ['90%', 'bold']],
-	[c4_item_a => []],
-	[c4_item_a_title => 'bold'],
-	[c4_item_a_visited => []],
-	[c4_item_a_hover => ['underline']],
-	[c4_events_item => []],
-	[c4_venue => ['80%']],
-	[c4_copy => ['80%']],
-	[c4_site_tag => ['bold']],
-	[c4_tm => ['60%', 'style=vertical-align: top; line-height: 90%']],
-	[c4_pager => ['left']],
-	[c4_pager_month => ['80%', 'uppercase']],
-	[c4_pager_a => ['80%']],
-	[c4_featured => ['100%']],
-#TODO: needed for mobile only
-	[c4_button => ['100%']],
-	[c4_site_local => ['bold', 'family="American Typewriter", Courier']],
-	[c4_site_tag => ['family="American Typewriter", Courier']],
-	[c4_site_name => ['bold', 'family="American Typewriter", Courier']],
-	[c4_button => ['100%']],
-	[c4_home_list_title => ['120%', 'bold', 'center']],
     ],
     Task => [
 	[C4_HOME_LIST => 'search'],
 	[C4_HOME_USER_TRACKING => '/pub/url'],
 	[C4_HOME_SUGGEST_SITE => '/pub/suggest-site'],
+	[SITE_WIKI_VIEW => undef],
 	[C4_HOME_WIKI_VIEW => '/bp/*'],
 	[LOGIN => undef],
     ],
@@ -114,33 +55,22 @@ my($_SELF) = __PACKAGE__->new({
 	    C4_HOME_SUGGEST_SITE => 'Comments or Suggestions?',
 	    c4_about => 'About',
 	    C4_HOME_LIST => 'Home',
+	    C4_HOME_WIKI_VIEW => 'Wiki',
 	]],
 	[SuggestSiteForm => [
 	    'prose.prologue' => q{BR(); Know of a local venue we don't cover? Let us know!BR();BR(); Any other comments, questions, or suggestions are appreciated.BR();BR();},
-	    'suggestion' => '',
+	    'suggestion' => 'Message',
 	    'email' => 'Your email (optional)',
 	    ok_button => 'Submit',
 	]],
 	[previous_button => 'Previous'],
 	[next_button => 'Next'],
+	[prose => [
+	    xhtml_copyright => <<"EOF",
+&copy; @{[$_DT->now_as_year]} vs_text('site_copyright'); BR(); SPAN_c4_site_tag('Make a ');SPAN_c4_site_local('local');SPAN_c4_site_tag(' scene.&trade;');
+EOF
+	]],
     ],
 });
-
-sub internal_merge {
-    my($proto) = shift;
-    my($cfg) = $proto->SUPER::internal_merge(@_);
-    my($base_tasks) = $proto->internal_base_tasks;
-    $cfg->{Task} = [
-    	map(
-	    {
-		my($t) = $_->[0];
-		grep($t eq $_->[0], @{$base_tasks})
-		    || $t =~ /^C4_HOME_|MAIL_RECEIVE|XAPIAN/ ? $_ : (),
-	    }
-    	    @{$cfg->{Task}},
-    	),
-    ];
-    return $cfg;
-}
 
 1;
