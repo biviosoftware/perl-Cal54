7b617805c4795c75dfd97da83a015c2c0cb829e5
Paul Moeller <moeller@bivio.biz>
2014-07-02 19:17:33 +0000

    removed overrides

--- Facade/Mobile.pm
+++ Facade/Mobile.pm
@@ -12,22 +12,8 @@ __PACKAGE__->new({
     http_host => 'm.cal54.com',
     mail_host => 'm.cal54.com',
     uri => 'm.cal54',
-    CSS => [
-	[c4_query_what => 'width: 14em;'],
-	[c4_form => ''],
-	[c4_list => ''],
-	[c4_home_bottom_pager => ''],
-    ],
     Constant => [
 	[robots_txt_allow_all => 0],
-    ],
-    Font => [
-	[c4_home => ['family=Arial, Helvetica, sans-serif', '100%']],
-	[[qw(c4_logo_tag c4_logo_name)] => [qw(bold uppercase 120%)]],
-	[c4_query_what => ['100%']],
-	[c4_query_submit => ['80%']],
-	[c4_pager_month => ['70%', 'uppercase']],
-	[c4_pager_a => ['70%']],
     ],
 });
 
