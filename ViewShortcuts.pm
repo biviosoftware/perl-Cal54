7ee8a0f12212b5ace499693d9aeefcff6a144f21
Paul Moeller <moeller@bivio.biz>
2014-07-02 19:37:49 +0000

    removed vs_unless_robot()

--- ViewShortcuts.pm
+++ ViewShortcuts.pm
@@ -1,14 +1,9 @@
 # Copyright (c) 2010 CAL54, Inc.  All rights reserved.
 # $Id$
 package Cal54::ViewShortcuts;
-use Bivio::Base 'Bivio::UI::XHTML::ViewShortcuts';
+use Bivio::Base 'Bivio::UI::Bootstrap::ViewShortcuts';
 use Bivio::UI::ViewLanguageAUTOLOAD;
 
 our($VERSION) = sprintf('%d.%02d', q$Revision$ =~ /\d+/g);
-
-sub vs_unless_robot {
-    my(undef, $widget, $else) = @_;
-    return If(['!', ['->req', 'Type.UserAgent'], '->is_robot'], $widget, $else);
-}
 
 1;
